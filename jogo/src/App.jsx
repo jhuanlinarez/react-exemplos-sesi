6e4bfb1e9bf625fb35e0c5840dc4d7a65d956138
jhuanlinarez <linarezjhuan721@gmail.com>
2024-03-08 14:55:37 -0300

    a

--- jogo/src/App.jsx
+++ jogo/src/App.jsx
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="App">
-      <h1>Jogo da Velha</h1>
+      <h1 className="game-title">Jogo da Velha</h1>
       <Board />
     </div>
   );
