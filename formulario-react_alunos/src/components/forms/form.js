8c427e1ceb79883042f9eeb9403ce27743b63aca
jhuanlinarez <linarezjhuan721@gmail.com>
2024-03-22 08:29:24 -0300

    aa

--- formulario-react_alunos/src/components/forms/form.js
+++ formulario-react_alunos/src/components/forms/form.js
@@ -1,61 +1,52 @@
-//Importa o React e os hooks necessários do React
 import React, {useState} from 'react';
-//Importa o hook useForm do react-hook-form para lidar com formulários de maneira eficiente
-//Obs:Para instalar (npm install react-hook-form)
 import {useForm} from 'react-hook-form';
-//importa o arquivo de estilos CSS
 import '../css/style.css';
-//importa a imagem de fundo do login
 import minhaImagem from '../images/login.jpg';
-//importa a imagem de verificação
 import verified from '../images/verified.png';
-//função principal que representa o componente do formulário
-function MyForm() {
-    //destruturação do objeto retornado pelo hook useForm
-    //formState é um onjeto que contém o estado do formulário, e erros é uma propriedade desse objeto
-    const { register, handleSubmit, formState: {errors} } = useForm();
-    //estado local para controlar o estado do formulário
-    const [status, setStatus] = useState({submittedSuccessfully: false, loading: false, showPassword: false});
-//função que é chamada quando o formulário é enviado
-    const onSubmit = data => {
-        //{...status}: O operador de propagação (...) é utilizado para criar uma cópia do estado (status). Isso é feito para garantir que não estamos modificando diretamente o estado anteriormente respeitando o príncipio de imutabilidade no React.
-        setStatus({...status, loading: true}) //Atualiza o estado para indicar que está a carregar
 
-        //simula um tempo de espera antes de atualizar o estado para sucesso
+//Função principal que representa o componetne de fornulário
+function MyForm() {
+    const { register, handleSubmit, formState: {errors}} = useForm();
+    //Para controlar o estado do formulário
+    const [status, setStatus] = useState({submittedSuccessfully: false, loading: false, showPassword: false});
+
+    //Função que é chamada quando o formulario é enviado
+    const onSubmit = data => {
+       setStatus({...status, loading: true});//Atualiza o estado para indicar que está carregando
+
+
+        //Simula um tempo de espera antes de atualizar o estado para sucesso
         setTimeout(() => {
-            setStatus({ submitedSuccessfully: true, loading: false});
+            setStatus({ submittedSuccessfully: true, loading: false});
         }, 1000);
     };
-//função para gerar mensagens de erro com base no nome do campo
-    const generateErrorMessage = fieldName => {
+
+    //Função para gerar mensagens de erro com base no nome do campo
+    const generateErrorMessage = fielName => {
         return {
-            required: `${fieldName} é obrigatório`, //mensagem para campos obrigatótios
-            pattern: fieldName === 'email' ? 'Formato de e-mail enválido' : null //mensagem para formato de e-mail inválido
+            required: `${fielName} é obrigatório`, //Mensagem para campos obrigatórios
+            pattern: fielName === 'email'? 'Formato de e-mail inválido': null //Mensagem para formulário de e-mail inválido 
         };
     };
 
-    //função para lidar com o retorno á página inicial
+    //Função para lidar com o retorno à página inicial
     const handleReturnButtonClick = () => {
-        //redireciona para a pagina inicial
+        //Redireciona para a página inicial
         window.location.href = '/';
     };
 
-
-
-
-    //renderização condicional (usando ternário ?) com base no sucesso do envio do formulário
+    //Renderização condicional (usando ternário ?) com base no sucesso de envio do formulário
     return (
         <div className='container'>
             {status.submittedSuccessfully ? (
-                //se o formulário foi enviado com sucesso, exibe uma mensagem de sucesso
-                <div className='success-message'>
+                 <div className='success-message'>
                     <img src={verified} alt='Verificado' />
                     <h2>Formulário Enviado com Sucesso!</h2>
                     <p>Obrigado por enviar o formulário.</p>
                     <button className='btn' onClick={handleReturnButtonClick}>Retornar</button>
-                </div>
-            ) : (
-                //se o formuláro não foi enviado com sucesso, exibe o formulário para preenchimento
+                 </div>
+            ):(
+                //Se o formulário ainda não foi enviado co sucesso, exibe o formulário para preenchimento
                 <div className='form-sign-up'>
                     <div>
                         <img src={minhaImagem} alt='imagem de uma pessoa se cadastrando pelo celular' />
@@ -64,13 +55,14 @@ function MyForm() {
                     <section>
                         <h1>Inscreva-se</h1>
 
-                        {/*Formulário com campos controlados pelo hook useForm*/}
+                        {/* Formulário com campo controlado pelo hook useForm*/}
                         <form onSubmit={handleSubmit(onSubmit)}>
-                            {/*Campo Nome*/}
+                        
+                        {/* Campo nome*/}
                             <div className='form flex'>
-                                <label htmlFor='nome'>Nome</label>
-                                <input
-                                {...register("firtsName", generateErrorMessage("Nome"))}
+                                <label htmlFor='name'>Nome</label>
+                                <input 
+                                {...register("firstName", generateErrorMessage("Nome"))}
                                 placeholder="Nome"
                                 id='name'
                                 autoComplete='off'
@@ -79,10 +71,11 @@ function MyForm() {
                                 {errors.firstName && <span className='error-message'>{errors.firstName.message}</span>}
                             </div>
 
-                            {/*Campo Sobrenome*/}
-                            <div className='form flex'>
+
+                             {/* Campo sobrenome*/}
+                             <div className='form flex'>
                                 <label htmlFor='sobrenome'>Sobrenome</label>
-                                <input
+                                <input 
                                 {...register("lastName", generateErrorMessage("Sobrenome"))}
                                 placeholder="Sobrenome"
                                 id='sobrenome'
@@ -92,24 +85,25 @@ function MyForm() {
                                 {errors.lastName && <span className='error-message'>{errors.lastName.message}</span>}
                             </div>
 
-                            {/*Campo email*/}
-                            <div className='form flex'>
-                                <label htmlFor='email'>E-mail</label>
-                                <input
+
+                             {/* Campo E-mail*/}
+                             <div className='form flex'>
+                                <label htmlFor='email'>Email</label>
+                                <input 
                                 {...register("email", generateErrorMessage("E-mail"))}
                                 placeholder="E-mail"
                                 id='email'
                                 autoComplete='off'
-                                className={errors.email ? 'error' : ''}
+                                className={errors.firstName ? 'error' : ''}
                                 />
                                 {errors.email && <span className='error-message'>{errors.email.message}</span>}
                             </div>
 
-                            {/*Campo senha*/}
-                            <div className='form flex'>
+                             {/* Campo senha*/}
+                             <div className='form flex'>
                                 <label htmlFor='password'>Senha</label>
                                 <div className="password-input-container">
-                                <input
+                                <input 
                                 {...register("password", generateErrorMessage("Senha"))}
                                 placeholder="Senha"
                                 id='password'
@@ -118,24 +112,23 @@ function MyForm() {
                                 className={errors.password ? 'error' : ''}
                                 />
 
-                                <button
-                                type="button" //define o tipo de botão com "button" para evitar comportamento padrão de envio de formulário
-                                className='password-toggle'//adiciona a classe CSS 'password-toggle' para estilização específica
+                                <button 
+                                type="button"
+                                className='password-toggle'
                                 onClick={() => setStatus({...status, showPassword: !status.showPassword})}
-                                //define a função de clique que atualiza o estado 'status', alternando o valor de 'showPassword'
                                 >
 
-                            {status.showPassword ? <i className="fas fa-eye"></i> : <i className="fas fa-eye-slash"></i>}
-                                {/*renderiza condicionalemnte um ícone de olho abertou ou fechado com base no valor de 'showPassword'*/}
+                                    {status.showPassword ? <i className="fas fa-eye"></i> : <i className='fas fa-eye-slash'></i>}
+                                    
                                 </button>
-                               
-                        </div>
-                                {errors.password && <span className='error-message'>{errors.password.message}</span>}
-                            </div>
+                                </div>
+                               {errors.password && <span className='error-message'>{errors.password.message}</span>}
+                             </div>
 
-                            {/*Botão enviar*/}
-                            <button className='btn' type="submit" disabled={status.loading}>{status.loading ? 'Enviando...' : 'Enviar'}
-                            </button>
+                             {/* Botãp Enviar */}
+                             <button className='btn' type="submit" disabled={status.loading}>
+                                {status.loading ? 'Enviando...': 'Enviar'}
+                             </button>
                         </form>
                         {status.loading && <p>Carregando...</p>}
                     </section>
@@ -143,6 +136,8 @@ function MyForm() {
             )}
         </div>
     );
+
+
 }
 
 //Exporta componente "MyForm"
